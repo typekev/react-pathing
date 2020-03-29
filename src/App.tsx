929e3b2dda086abc35cacf797bf82dfd7f52ba5e
Kevin Gonzalez <typekev@gmail.com>
2020-03-30 00:04:50 +0200

    render board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Main from "./components/Main";
+import Board from "./components/Board";
 
 function App() {
-  return <main></main>;
+  return (
+    <Main>
+      <Board />
+    </Main>
+  );
 }
 
 export default App;
