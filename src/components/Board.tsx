f9cca0c1ee7a4810604679ee612756aecdde50f7
Kevin Gonzalez <typekev@gmail.com>
2020-04-03 23:29:19 +0200

    remove  unused import

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import Cell from "./Cell";
 import { WINDOW_PADDING, CELL_WIDTH } from "../constants";
 import { addEventListeners } from "../utils";
