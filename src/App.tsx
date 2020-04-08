7810ad2f7ff17ba84a40568f4ebd344976910fa4
Kevin Gonzalez <typekev@gmail.com>
2020-04-08 23:38:03 +0200

    Add empty mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core";
 import Main from "./components/Main";
 import Board from "./components/Board";
+import theme from "./theme";
 
 function App() {
   return (
-    <Main>
-      <Board />
-    </Main>
+    <ThemeProvider theme={theme}>
+      <Main>
+        <Board />
+      </Main>
+    </ThemeProvider>
   );
 }
 
