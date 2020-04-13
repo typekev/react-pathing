f40cf08808a35ba828c9bc3ea6ad6097d181f2f8
Kevin Gonzalez <typekev@gmail.com>
2020-04-13 22:52:14 +0200

    add cell style for pathmode

--- src/components/Cell.ts
+++ src/components/Cell.ts
@@ -1,9 +1,9 @@
-import styled, { css } from 'styled-components'
-import { NODE_WIDTH } from '../constants'
-import { MODES } from '../types'
+import styled, { css } from 'styled-components';
+import { NODE_WIDTH } from '../constants';
+import { MODES } from '../types';
 
 interface Props {
-  mode: MODES
+  mode: MODES;
 }
 
 const Cell = styled.div`
@@ -15,26 +15,33 @@ const Cell = styled.div`
   margin-bottom: -0.0625rem;
   margin-right: -0.0625rem;
 
- ${({ mode }: Props) =>
-   mode === MODES.FILL_MODE &&
-   css`
-     background-color: black;
-     border-color: black;
-   `}
+  ${({ mode }: Props) =>
+    mode === MODES.FILL_MODE &&
+    css`
+      background-color: black;
+      border-color: black;
+    `}
 
- ${({ mode }: Props) =>
-   mode === MODES.START_NODE_MODE &&
-   css`
-     background-color: green;
-     border-color: green;
-   `}
+  ${({ mode }: Props) =>
+    mode === MODES.START_NODE_MODE &&
+    css`
+      background-color: green;
+      border-color: green;
+    `}
 
- ${({ mode }: Props) =>
-   mode === MODES.TARGET_NODE_MODE &&
-   css`
-     background-color: red;
-     border-color: red;
-   `}
-`
+  ${({ mode }: Props) =>
+    mode === MODES.TARGET_NODE_MODE &&
+    css`
+      background-color: red;
+      border-color: red;
+    `}
 
-export default Cell
+  ${({ mode }: Props) =>
+    mode === MODES.PATH_NODE_MODE &&
+    css`
+      background-color: yellowgreen;
+      border-color: yellowgreen;
+    `}
+`;
+
+export default Cell;
