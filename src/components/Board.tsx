f75a8f9d6ed427dee6f053bcfe128ec44c83597b
Kevin Gonzalez <typekev@gmail.com>
2020-04-15 21:01:47 +0200

    make seperate clearPath  func

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,6 +10,22 @@ import handleNodeSelect from './Board/handleNodeSelect';
 import initBoard from './Board/initBoard';
 import dijkstra from '../pathers/dijkstra';
 
+const clearPaths = async (
+  grid: Node[][],
+  setGrid: React.Dispatch<React.SetStateAction<Node[][]>>,
+) =>
+  Promise.resolve(grid.flat().filter(node => node.mode === MODES.PATH_NODE_MODE))
+    .then(pathNodes => {
+      const nextGrid = [...grid];
+
+      pathNodes.forEach(({ x, y, index }) => {
+        nextGrid[x][y] = { x, y, index, mode: MODES.CLEAR_MODE };
+      });
+
+      return nextGrid;
+    })
+    .then(nextGrid => setGrid(nextGrid));
+
 interface Props {
   grid: Node[][];
   setGrid: React.Dispatch<React.SetStateAction<Node[][]>>;
@@ -41,15 +57,12 @@ const Board = ({ grid, setGrid, mainElement }: Props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [grid, setGrid]);
 
-  const runDijkstra = () => {
-    if (startNode && targetNode) {
+  const runDijkstra = () =>
+    startNode &&
+    targetNode &&
+    clearPaths(grid, setGrid).then(() => {
       const path = dijkstra({ startNode, endNode: targetNode, grid });
-      const pathNodes = flatGrid.filter(node => node.mode === MODES.PATH_NODE_MODE);
       const nextGrid = [...grid];
-
-      pathNodes.forEach(({ x, y, index }) => {
-        nextGrid[x][y] = { x, y, index, mode: MODES.CLEAR_MODE };
-      });
 
       path.shift();
       path.pop();
@@ -58,8 +71,7 @@ const Board = ({ grid, setGrid, mainElement }: Props) => {
       });
 
       setGrid(nextGrid);
-    }
-  };
+    });
 
   return (
     <BoardSection>
