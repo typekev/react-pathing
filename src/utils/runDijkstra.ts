731ee0330a55c4f6fa0de9d2aa7cce9927447124
Kevin Gonzalez <typekev@gmail.com>
2020-04-19 00:08:56 +0200

    fix pathIndex bug

--- src/utils/runDijkstra.ts
+++ src/utils/runDijkstra.ts
@@ -14,15 +14,16 @@ const runDijkstra = (
   clearPaths(grid, setGrid).then(clearedGrid => {
     const path = dijkstra({ startNode, endNode: targetNode, grid });
     const nextGrid = cloneDeep(clearedGrid);
+    let i = 0;
 
     path.shift();
     path.pop();
-    path.filter(Boolean).forEach(({ x, y, index }, pathIndex) => {
+    path.filter(Boolean).forEach(({ x, y, index }) => {
       nextGrid[x][y] = {
         x,
         y,
         index,
-        pathIndex: grid[x][y].mode === MODES.PATH_NODE_MODE ? 0 : pathIndex,
+        pathIndex: grid[x][y].mode === MODES.PATH_NODE_MODE ? 0 : i++,
         mode: MODES.PATH_NODE_MODE,
       };
     });
