1497ab91f0f3f4173b4f596cca585461d1a9e383
Kevin Gonzalez <typekev@gmail.com>
2020-06-07 22:52:01 +0200

    Add options for pather

---
+++ src/components/OptionsDialog/PatherOptions.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Pathers, Options } from '../../types';
+import SelectField from '../SelectField';
+
+interface Props {
+  options: Options;
+  setOptions: React.Dispatch<React.SetStateAction<Options>>;
+}
+
+const selectFieldOptions = Object.entries(Pathers).map(([label, value]) => ({ label, value }));
+
+const PatherOptions = ({ options, setOptions }: Props) => (
+  <SelectField
+    label="Select a path finding algorithm"
+    options={selectFieldOptions}
+    value={selectFieldOptions.find(({ value }) => value === options.pather)}
+    onChange={option =>
+      setOptions({ ...options, pather: (option?.value as Pathers) || options.pather })
+    }
+  />
+);
+
+export default PatherOptions;
