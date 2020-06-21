cd7060b8a9979703762690fdb5ce76f416b9fd7d
Kevin Gonzalez <typekev@gmail.com>
2020-06-21 22:46:39 +0200

    add palette

---
+++ src/palette.ts
@@ -0,0 +1,19 @@
+export const primary = {
+    main: '#c1dfff',
+    dark: '#90adcc',
+    light: '#f4ffff',
+}
+
+export const secondary = {
+    main: '#fff869',
+    light: '#ffff9b',
+    dark: '#c9c536',
+}
+
+export const error = {
+    main: '#db2624',
+}
+
+export const background = {
+    default: '#fdf9f6',
+}
